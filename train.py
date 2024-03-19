48ab6d44351d12381bd0df9164ae20e38d947ffb
Propack007 <pravin.hkay@gmail.com>
2024-03-19 12:21:29 -0400

    test files

--- train.py
+++ train.py
@@ -1,302 +1,88 @@
-from tensorflow.python.ops.numpy_ops import np_config
-import pickle
-from os.path import isfile, join
-from os import listdir
-from tensorflow.keras.layers.experimental.preprocessing import StringLookup
-from tensorflow import keras
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import re
+from tensorflow.python.keras.models import load_model
 import numpy as np
-import os
-
-np.random.seed(42)
-tf.random.set_seed(42)
-
-base_path = "IAM_Words"
-words_list = []
-
-words = open(f"{base_path}/words.txt", "r").readlines()
-for line in words:
-    if line[0] == "#":
-        continue
-    if line.split(" ")[1] != "err":
-        words_list.append(line)
-
-len(words_list)
-
-np.random.shuffle(words_list)
-
-print(words_list[0:10])
-
-
-split_idx = int(0.9*len(words_list))
-train_samples = words_list[:split_idx]
-test_samples = words_list[split_idx:]
-
-val_split_idx = int(0.5 * len(test_samples))
-validation_samples = test_samples[:val_split_idx]
-test_samples = test_samples[val_split_idx:]
-
-assert len(words_list) == len(train_samples) + \
-    len(validation_samples) + len(test_samples)
-
-print(f"Total Training Samples: {len(train_samples)}")
-print(f"Total validation samples: {len(validation_samples)}")
-print(f"Total test samples: {len(test_samples)}")
-
-base_image_path = os.path.join(base_path, "words")
-print(base_path)
-
-
-def get_image_paths_and_labels(samples):
-    paths = []
-    corrected_samples = []
-    for (i, file_line) in enumerate(samples):
-        line_split = file_line.strip()
-        line_split = line_split.split(" ")
-
-        # Each line split willl have this format for the the corresponding image:
-        # part1/part1-part2/part1-part2-part3.png
-        image_name = line_split[0]
-        partI = image_name.split("-")[0]
-        partII = image_name.split("-")[1]
-        img_path = os.path.join(
-            base_image_path, partI, partI + "-" + partII, image_name + ".png"
-        )
-        if os.path.getsize(img_path):
-            paths.append(img_path)
-            corrected_samples.append(file_line.split("\n")[0])
-
-    return paths, corrected_samples
-
-
-train_img_paths, train_labels = get_image_paths_and_labels(train_samples)
-validation_img_paths, validation_labels = get_image_paths_and_labels(
-    validation_samples)
-test_img_paths, test_labels = get_image_paths_and_labels(test_samples)
-
-
-base_image_path = os.path.join(
-    base_path, "/content/drive/MyDrive/Colab Notebooks/OCR_course/ocr_dataset_v1/test_imgs")
-print(base_image_path)
-
-t_images = []
-
-# onlyfiles = [f for f in listdir(base_image_path) if isfile(join(base_image_path, f))]
-for f in listdir(base_image_path):
-    t_images_path = os.path.join(base_image_path, f)
-    t_images.append(t_images_path)
-
-print(t_images)
-
-t_images[0:10]
-
-
-def atoi(text):
-    return int(text) if text.isdigit() else text
-
-
-def natural_keys(text):
-    '''
-    alist.sort(key=natural_keys) sorts in human order
-    http://nedbatchelder.com/blog/200712/human_sorting.html
-    (See Toothy's implementation in the comments)
-    '''
-    return [atoi(c) for c in re.split(r'(\d+)', text)]
-
-# alist=[
-#     "something1",
-#     "something12",
-#     "something17",
-#     "something2",
-#     "something25",
-#     "something29"]
-
-
-t_images.sort(key=natural_keys)
-print(t_images)
-
-train_img_paths[0:10]
-
-train_labels[0: 10]
-
-train_labels_cleaned = []
-characters = set()
-max_len = 0
-
-for label in train_labels:
-    label = label.split(" ")[-1].strip()
-    for char in label:
-        characters.add(char)
-
-    max_len = max(max_len, len(label))
-    train_labels_cleaned.append(label)
-
-print("Maximum length: ", max_len)
-print("Vocab size: ", len(characters))
-
-# Check some label samples
-train_labels_cleaned[:10]
-
-
-def clean_labels(labels):
-    cleaned_labels = []
-    for label in labels:
-        label = label.split(" ")[-1].strip()
-        cleaned_labels.append(label)
-    return cleaned_labels
-
-
-validation_labels_cleaned = clean_labels(validation_labels)
-test_labels_cleaned = clean_labels(test_labels)
-
-ff = list(characters)
-
-with open("/content/drive/MyDrive/Colab Notebooks/OCR_course/characters", "rb") as fp:   # Unpickling
-    b = pickle.load(fp)
-    print(b)
-
-AUTOTUNE = tf.data.AUTOTUNE
-
-# Maping characaters to integers
-char_to_num = StringLookup(vocabulary=b, mask_token=None)
-
-# Maping integers back to original characters
-num_to_chars = StringLookup(
-    vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True)
-
-
-def distortion_free_resize(image, img_size):
-    w, h = img_size
-    image = tf.image.resize(image, size=(h, w), preserve_aspect_ratio=True)
-
-    # Check tha amount of padding needed to be done.
-    pad_height = h - tf.shape(image)[0]
-    pad_width = w - tf.shape(image)[1]
-
-    # only necessary if you want to do same amount of padding on both sides.
-    if pad_height % 2 != 0:
-        height = pad_height // 2
-        pad_height_top = height + 1
-        pad_height_bottom = height
-    else:
-        pad_height_top = pad_height_bottom = pad_height // 2
-
-    if pad_width % 2 != 0:
-        width = pad_width // 2
-        pad_width_left = width + 1
-        pad_width_right = width
-    else:
-        pad_width_left = pad_width_right = pad_width // 2
-
-    image = tf.pad(
-        image, paddings=[
-            [pad_height_top, pad_height_bottom],
-            [pad_width_left, pad_width_right],
-            [0, 0],
-        ],
-    )
-    image = tf.transpose(image, perm=[1, 0, 2])
-    image = tf.image.flip_left_right(image)
-    return image
-
-
-batch_size = 64
-padding_token = 99
-image_width = 128
-image_height = 32
-
-
-def preprocess_image(image_path, img_size=(image_width, image_height)):
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_png(image, 1)
-    image = distortion_free_resize(image, img_size)
-    image = tf.cast(image, tf.float32) / 255.0
-    return image
-
-
-def vectorize_label(label):
-    label = char_to_num(tf.strings.unicode_split(
-        label, input_encoding="UTF-8"))
-    length = tf.shape(label)[0]
-    pad_amount = max_len - length
-    label = tf.pad(label, paddings=[[0, pad_amount]],
-                   constant_values=padding_token)
-    return label
-
-
-def process_images_labels(image_path, label):
-    image = preprocess_image(image_path)
-    label = vectorize_label(label)
-    return {"image": image, "label": label}
-
-
-def prepare_dataset(image_paths, labels):
-    dataset = tf.data.Dataset.from_tensor_slices((image_paths, labels)).map(
-        process_images_labels, num_parallel_calls=AUTOTUNE
-    )
-
-    return dataset.batch(batch_size).cache().prefetch(AUTOTUNE)
-
-
-train_ds = prepare_dataset(train_img_paths, train_labels_cleaned)
-validation_ds = prepare_dataset(
-    validation_img_paths, validation_labels_cleaned)
-test_ds = prepare_dataset(test_img_paths, test_labels_cleaned)
-
-batch_size = 64
-padding_token = 99
-image_width = 128
-image_height = 32
-
-
-def preprocess_image(image_path, img_size=(image_width, image_height)):
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_png(image, 1)
-    image = distortion_free_resize(image, img_size)
-    image = tf.cast(image, tf.float32) / 255.0
-    return image
-
-
-def process_images_2(image_path):
-    image = preprocess_image(image_path)
-    # label = vectorize_label(label)
-    return {"image": image}
-
-
-def prepare_test_images(image_paths):
-    dataset = tf.data.Dataset.from_tensor_slices((image_paths)).map(
-        process_images_2, num_parallel_calls=AUTOTUNE
-    )
-
-    # return dataset
-    return dataset.batch(batch_size).cache().prefetch(AUTOTUNE)
-
-
-inf_images = prepare_test_images(t_images)
-
-np_config.enable_numpy_behavior()
-
-for data in inf_images.take(1):
-    images = data["image"]
-    # imm = images.reshape(images.shape[0], (images.shape[1]*images.shape[2]))
-    # imm = imm.transpose()
-    # print(imm.shape)
-    _, ax = plt.subplots(4, 4, figsize=(15, 8))
-
-    # ss = plt.imshow(imm, cmap="gray")
-    # plt.show()
-    for i in range(16):
-        img = images[i]
-        # print(img.shape)
-        img = tf.image.flip_left_right(img)
-        img = tf.transpose(img, perm=[1, 0, 2])
-        img = (img * 255.0).numpy().clip(0, 255).astype(np.uint8)
-        img = img[:, :, 0]
-
-        # Gather indices where Label!= padding token
-
-        ax[i // 4, i % 4].imshow(img, cmap="gray")
-
-    plt.show()
+import pandas as pd
+from glob import glob
+import keras
+import matplotlib.pyplot as plt
+from tensorflow.python.keras.layers import Input, Lambda, Dense, Flatten
+from tensorflow.python.keras.models import Model
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.models import Sequential
+
+train_path = 'Datasets/train'
+valid_path = 'Datasets/test'
+test_path = 'Datasets/val'
+
+IMAGE_SIZE = [224, 224]  # Default image size for VGG16
+
+folders = glob('Datasets/train/*')  # Get number of classes
+
+
+# ImageDataGenerator can help perform augumentation, get more diverse train set.
+train_datagen = ImageDataGenerator(
+    rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+validation_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# Through flow_from_directory - array of images that can be used for training.
+training_set = train_datagen.flow_from_directory('Datasets/train',
+                                                 target_size=(224, 224),
+                                                 batch_size=64,
+                                                 class_mode='categorical')
+
+validation_set = validation_datagen.flow_from_directory('Datasets/val',
+                                                        target_size=(224, 224),
+                                                        batch_size=64,
+                                                        class_mode='categorical')
+
+test_set = test_datagen.flow_from_directory('Datasets/test',
+                                            target_size=(224, 224),
+                                            batch_size=32,
+                                            class_mode='categorical')
+
+# Create a VGG16 model, and removing the last layer that is classifying 1000 images. This will be replaced with images classes we have.
+# Training with Imagenet weights
+vgg = VGG16(input_shape=IMAGE_SIZE + [3],
+            weights='imagenet', include_top=False)
+
+# Use this line for VGG19 network. Create a VGG19 model, and removing the last layer that is classifying 1000 images. This will be replaced with images classes we have.
+# vgg = VGG19(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+
+# This sets the base that the layers are not trainable. If we'd want to train the layers with custom data, these two lines can be ommitted.
+for layer in vgg.layers:
+    layer.trainable = False
+
+x = Flatten()(vgg.output)  # Output obtained on vgg16 is now flattened.
+# We have 5 classes, and so, the prediction is being done on len(folders) - 5 classes
+prediction = Dense(len(folders), activation='softmax')(x)
+
+# Creating model object
+model = Model(inputs=vgg.input, outputs=prediction)
+
+model.summary()
+
+# Compile the model
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam', metrics=['accuracy'])
+history = model.fit(
+    training_set, validation_data=validation_set, epochs=20, batch_size=32)
+
+# loss
+plt.plot(history.history['loss'], label='train loss')
+plt.plot(history.history['val_loss'], label='val loss')
+plt.legend()
+plt.show()
+plt.savefig('LossVal_loss')
+
+# accuracies
+plt.plot(history.history['accuracy'], label='train acc')
+plt.plot(history.history['val_accuracy'], label='val acc')
+plt.legend()
+plt.show()
+plt.savefig('AccVal_acc')
+
+
+model.save('FlowerClassification_VGG16.h5')
